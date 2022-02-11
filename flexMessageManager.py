adb5b91f3540cb0e6a3c8af9eaae8b272b56efb1
NK.Shien <shien091091@gmail.com>
2022-02-12 01:13:02 +0800

    調整FlexMessageFormat

--- flexMessageManager.py
+++ flexMessageManager.py
@@ -2,88 +2,48 @@
 class flexMessageManager:
 
     def getFlexMessage(str_title, str_statusMessage, str_content):
-        return """{
-        "type": "bubble",
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "text",
-                "text": "總長度最多可到",
-                "size": "xl",
-                "weight": "bold",
-                "color": "#587cbe"
-            }
-            ]
-        },
-        "hero": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "text",
-                "text": "[StatusMassage]",
-                "offsetStart": "xxl",
-                "color": "#5e637e"
-            }
-            ]
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "text",
-                "text": "1.AAA\n2.BBB",
-                "wrap": true,
-                "color": "#666666",
-                "size": "sm",
-                "flex": 5
-            }
-            ]
-        }
-        }"""
-        # return \
-        # f'"{{ \
-        # "type": "bubble", \
-        # "header": {{ \
-        #     "type": "box", \
-        #     "layout": "vertical", \
-        #     "contents": [ \
-        #     {{ \
-        #         "type": "text", \
-        #         "text": "{str_title}", \
-        #         "size": "xl", \
-        #         "weight": "bold", \
-        #         "color": "#587cbe" \
-        #     }} \
-        #     ] \
-        # }}, \
-        # "hero": {{ \
-        #     "type": "box", \
-        #     "layout": "vertical", \
-        #     "contents": [ \
-        #     {{ \
-        #         "type": "text", \
-        #         "text": "{str_statusMessage}", \
-        #         "offsetStart": "xxl", \
-        #         "color": "#5e637e" \
-        #     }} \
-        #     ] \
-        # }}, \
-        # "body": {{ \
-        #     "type": "box", \
-        #     "layout": "vertical", \
-        #     "contents": [ \
-        #     {{ \
-        #         "type": "text", \
-        #         "text": "{str_content}", \
-        #         "wrap": true, \
-        #         "color": "#666666", \
-        #         "size": "sm", \
-        #         "flex": 5 \
-        #     }} \
-        #     ] \
-        # }} \
-        # }}"'
+        newContent = str_content.replace('\n', '\\n')
+
+        return \
+        f'"{{ \
+        "type": "bubble", \
+        "header": {{ \
+            "type": "box", \
+            "layout": "vertical", \
+            "contents": [ \
+            {{ \
+                "type": "text", \
+                "text": "{str_title}", \
+                "size": "xl", \
+                "weight": "bold", \
+                "color": "#587cbe" \
+            }} \
+            ] \
+        }}, \
+        "hero": {{ \
+            "type": "box", \
+            "layout": "vertical", \
+            "contents": [ \
+            {{ \
+                "type": "text", \
+                "text": "{str_statusMessage}", \
+                "offsetStart": "xxl", \
+                "color": "#5e637e" \
+            }} \
+            ] \
+        }}, \
+        "body": {{ \
+            "type": "box", \
+            "layout": "vertical", \
+            "contents": [ \
+            {{ \
+                "type": "text", \
+                "text": "{newContent}", \
+                "wrap": true, \
+                "color": "#666666", \
+                "size": "sm", \
+                "flex": 5 \
+            }} \
+            ] \
+        }} \
+        }}"'

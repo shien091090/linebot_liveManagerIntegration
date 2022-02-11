d24a86743334b7338596f3cf46e39848eed695a1
NK.Shien <shien091091@gmail.com>
2022-02-11 22:51:48 +0800

    修改檔案名稱

--- manage.py
+++ manage.py
@@ -2,12 +2,13 @@ import imp
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextSendMessage, TextMessage, FlexSendMessage
+from TextParser import TextParser
 from dateManager import dateManager
 from flexMessageManager import flexMessageManager
 from lineActionInfo import LineActionInfo
-from textParser import TextParser
+
 from flask import Flask, request, abort
-import keyWordSetting
+import KeyWordSetting
 import lineActionInfo
 import settings
 import json
@@ -43,60 +44,60 @@ def receiveMessage(event):
     receiveTxt = event.message.text
 
     #新增待辦事項
-    if TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MEMO_ADD):
+    if TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MEMO_ADD):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MEMO_ADD,
+            KeyWordSetting.TITLE_MEMO_ADD,
             REQUEST_TYPE_GAS,
-            {'action':lineActionInfo.API_ACTION_MEMO_ADD,'content':TextParser.getSubStringByKeyWord(receiveTxt, keyWordSetting.KEY_MEMO_ADD)})
+            {'action':lineActionInfo.API_ACTION_MEMO_ADD,'content':TextParser.getSubStringByKeyWord(receiveTxt, KeyWordSetting.KEY_MEMO_ADD)})
     
     #刪除待辦事項
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MEMO_REMOVE):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MEMO_REMOVE):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MEMO_REMOVE,
+            KeyWordSetting.TITLE_MEMO_REMOVE,
             REQUEST_TYPE_GAS,
-            {'action':lineActionInfo.API_ACTION_MEMO_REMOVE,'content':TextParser.getNumberByKeyWordAndNumber(receiveTxt, keyWordSetting.KEY_MEMO_REMOVE)})
+            {'action':lineActionInfo.API_ACTION_MEMO_REMOVE,'content':TextParser.getNumberByKeyWordAndNumber(receiveTxt, KeyWordSetting.KEY_MEMO_REMOVE)})
 
     #修改待辦事項
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MEMO_MODIFY):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MEMO_MODIFY):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MEMO_MODIFY,
+            KeyWordSetting.TITLE_MEMO_MODIFY,
             REQUEST_TYPE_GAS,
-            {'action':lineActionInfo.API_ACTION_MEMO_MODIFY,'content':TextParser.getNumberByKeyWordAndNumber(receiveTxt, keyWordSetting.KEY_MEMO_MODIFY)})
+            {'action':lineActionInfo.API_ACTION_MEMO_MODIFY,'content':TextParser.getNumberByKeyWordAndNumber(receiveTxt, KeyWordSetting.KEY_MEMO_MODIFY)})
 
     #確認待辦事項
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MEMO_CHECK):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MEMO_CHECK):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MEMO_CHECK,
+            KeyWordSetting.TITLE_MEMO_CHECK,
             REQUEST_TYPE_GAS,
             {'action':lineActionInfo.API_ACTION_MEMO_CHECK,'content':''})
 
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MONTHLY_MEMO_ADD):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MONTHLY_MEMO_ADD):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MONTHLY_MEMO_ADD,
+            KeyWordSetting.TITLE_MONTHLY_MEMO_ADD,
             REQUEST_TYPE_GAS,
             lineActionInfo.API_ACTION_MONTHLY_MEMO_ADD)
 
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MONTHLY_MEMO_REMOVE):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MONTHLY_MEMO_REMOVE):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MONTHLY_MEMO_REMOVE,
+            KeyWordSetting.TITLE_MONTHLY_MEMO_REMOVE,
             REQUEST_TYPE_GAS,
             lineActionInfo.API_ACTION_MONTHLY_MEMO_REMOVE)
 
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MONTHLY_MEMO_MODIFY):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MONTHLY_MEMO_MODIFY):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MONTHLY_MEMO_MODIFY,
+            KeyWordSetting.TITLE_MONTHLY_MEMO_MODIFY,
             REQUEST_TYPE_GAS,
             lineActionInfo.API_ACTION_MONTHLY_MEMO_MODIFY)
 
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_MONTHLY_MEMO_CHECK):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_MONTHLY_MEMO_CHECK):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_MONTHLY_MEMO_CHECK,
+            KeyWordSetting.TITLE_MONTHLY_MEMO_CHECK,
             REQUEST_TYPE_GAS,
             lineActionInfo.API_ACTION_MONTHLY_MEMO_CHECK)
 
-    elif TextParser.checkHeaderByKeyWord(receiveTxt, keyWordSetting.KEY_BUY):
+    elif TextParser.checkHeaderByKeyWord(receiveTxt, KeyWordSetting.KEY_BUY):
         actionInfo = LineActionInfo(
-            keyWordSetting.TITLE_BUY,
+            KeyWordSetting.TITLE_BUY,
             REQUEST_TYPE_GAS,
             lineActionInfo.API_ACTION_BUY)
 

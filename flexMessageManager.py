4cce45a32951651c4689b4d2d61c645ffbe043f7
NK.Shien <shien091091@gmail.com>
2022-02-12 00:39:03 +0800

    test

--- flexMessageManager.py
+++ flexMessageManager.py
@@ -3,45 +3,88 @@ class flexMessageManager:
 
     def getFlexMessage(str_title, str_statusMessage, str_content):
         return \
-        f'"{{ \
-        "type": "bubble", \
-        "header": {{ \
-            "type": "box", \
-            "layout": "vertical", \
-            "contents": [ \
-            {{ \
-                "type": "text", \
-                "text": "{str_title}", \
-                "size": "xl", \
-                "weight": "bold", \
-                "color": "#587cbe" \
-            }} \
-            ] \
-        }}, \
-        "hero": {{ \
-            "type": "box", \
-            "layout": "vertical", \
-            "contents": [ \
-            {{ \
-                "type": "text", \
-                "text": "{str_statusMessage}", \
-                "offsetStart": "xxl", \
-                "color": "#5e637e" \
-            }} \
-            ] \
-        }}, \
-        "body": {{ \
-            "type": "box", \
-            "layout": "vertical", \
-            "contents": [ \
-            {{ \
-                "type": "text", \
-                "text": "{str_content}", \
-                "wrap": true, \
-                "color": "#666666", \
-                "size": "sm", \
-                "flex": 5 \
-            }} \
-            ] \
-        }} \
-        }}"'
+        """{
+        "type": "bubble",
+        "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "總長度最多可到十一個字",
+                "size": "xl",
+                "weight": "bold",
+                "color": "#587cbe"
+            }
+            ]
+        },
+        "hero": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "[StatusMassage]",
+                "offsetStart": "xxl",
+                "color": "#5e637e"
+            }
+            ]
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "1.AAA\n2.BBB",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+            }
+            ]
+        }
+        }"""
+        # return \
+        # f'"{{ \
+        # "type": "bubble", \
+        # "header": {{ \
+        #     "type": "box", \
+        #     "layout": "vertical", \
+        #     "contents": [ \
+        #     {{ \
+        #         "type": "text", \
+        #         "text": "{str_title}", \
+        #         "size": "xl", \
+        #         "weight": "bold", \
+        #         "color": "#587cbe" \
+        #     }} \
+        #     ] \
+        # }}, \
+        # "hero": {{ \
+        #     "type": "box", \
+        #     "layout": "vertical", \
+        #     "contents": [ \
+        #     {{ \
+        #         "type": "text", \
+        #         "text": "{str_statusMessage}", \
+        #         "offsetStart": "xxl", \
+        #         "color": "#5e637e" \
+        #     }} \
+        #     ] \
+        # }}, \
+        # "body": {{ \
+        #     "type": "box", \
+        #     "layout": "vertical", \
+        #     "contents": [ \
+        #     {{ \
+        #         "type": "text", \
+        #         "text": "{str_content}", \
+        #         "wrap": true, \
+        #         "color": "#666666", \
+        #         "size": "sm", \
+        #         "flex": 5 \
+        #     }} \
+        #     ] \
+        # }} \
+        # }}"'

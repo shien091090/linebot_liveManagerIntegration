36857d9e3a82d936c16f6f0613848a2218829ae6
NK.Shien <shien091091@gmail.com>
2022-02-14 00:32:10 +0800

    更新API URL : 修正刪掉最後一項待辦時ID會歸零的問題

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 LINE_CHANNEL_ACCESS_TOKEN = 'sPl0gKPsWSuq0Av7Us2F4Km8dLwDSuiYW75NmR9mpCRdug5Z9+a5psZ5iK/c3rgjdQfXp2L9iKi5vMG3QtRoAcT/G32RMhbHVUdMJZy1zeQ2D+7B7VHvMKnFLM/dZMe5zc/ukUCZ4YlA9j3lLA16/gdB04t89/1O/w1cDnyilFU='
 LINE_CHANNEL_SECRET = 'ab8d822858f40fa8a92abf575061211a'
-URL_GAS_API = 'https://script.google.com/macros/s/AKfycbxYu6yTJ4PbNsp1eXwmIiqXsEQuJUv-evMvZU7sFE1M81bJcnJQfb5wDrZGE-B873PpKw/exec'
+URL_GAS_API = 'https://script.google.com/macros/s/AKfycbx_GyjNgX_2AfYe7vhApP-WZnK8f1mSDo7370D_8edJobWWJWNsypZgnIrHOALiQPN2Ww/exec'

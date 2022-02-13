b2def57d0bb0f26211ce3f822dc9a4dbfb2c8b29
NK.Shien <shien091091@gmail.com>
2022-02-13 22:21:02 +0800

    更新API URL : 新增待辦列表為空時的回傳訊息

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 LINE_CHANNEL_ACCESS_TOKEN = 'sPl0gKPsWSuq0Av7Us2F4Km8dLwDSuiYW75NmR9mpCRdug5Z9+a5psZ5iK/c3rgjdQfXp2L9iKi5vMG3QtRoAcT/G32RMhbHVUdMJZy1zeQ2D+7B7VHvMKnFLM/dZMe5zc/ukUCZ4YlA9j3lLA16/gdB04t89/1O/w1cDnyilFU='
 LINE_CHANNEL_SECRET = 'ab8d822858f40fa8a92abf575061211a'
-URL_GAS_API = 'https://script.google.com/macros/s/AKfycbwDXx8m67xf7GlOOShcBlYBSLqAZ89xpIIweBAHUYuBAo4MB6JFRoM287dlRGcplYIDuQ/exec'
+URL_GAS_API = 'https://script.google.com/macros/s/AKfycbwFwI_7tKJo_pfeRgg8lbv2s9I0qetceU-D8oM8o4_gezqtNInQOXiDQm50Y3ADKU98tA/exec'

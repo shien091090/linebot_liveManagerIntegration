c505678162e9264325df41aadecb16ed7eb935b0
NKShien_Home <shien091091@gmail.com>
2022-07-19 00:24:22 +0800

    調整指令, 把多餘的空格刪掉

--- KeyWordSetting.py
+++ KeyWordSetting.py
@@ -1,14 +1,14 @@
 keyWordEnum = {
     'KEY_COMMAND_LIST': '指令',
-    'KEY_MEMO_ADD': '新增 ',
-    'KEY_MEMO_REMOVE': '刪除 ',
-    'KEY_MEMO_MODIFY': '修改 ',
+    'KEY_MEMO_ADD': '新增',
+    'KEY_MEMO_REMOVE': '刪除',
+    'KEY_MEMO_MODIFY': '修改',
     'KEY_MEMO_GET': '確認待辦',
-    'KEY_SCHEDULE_ADD': '新增行程 ',
-    'KEY_SCHEDULE_REMOVE': '刪除行程 ',
-    'KEY_SCHEDULE_MODIFY': '修改行程 ',
+    'KEY_SCHEDULE_ADD': '新增行程',
+    'KEY_SCHEDULE_REMOVE': '刪除行程',
+    'KEY_SCHEDULE_MODIFY': '修改行程',
     'KEY_SCHEDULE_GET': '確認行程',
-    'KEY_BUY': '買 '
+    'KEY_BUY': '買'
 }
 
 TITLE_COMMAND_LIST = '指令列表'

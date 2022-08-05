41a9f5c3670410ea93f79b9a3c743423259ff3dc
NKShien_Home <shien091091@gmail.com>
2022-08-05 08:49:05 +0800

    調整ParseText流程

--- TextParserManager.py
+++ TextParserManager.py
@@ -2,19 +2,150 @@ import dateManager
 
 DEFAULT_SPLIT_CHAR = ' '
 
+TextStructureType_Content = 'ContentType'
+TextStructureType_Number = 'NumberType'
+TextStructureType_Date = 'DateType'
+
 TextType_KeyWord = 'KeyWord'
 TextType_Number = 'Number'
 TextType_SubContent = 'SubContent'
 TextType_AdditionalContent = 'AdditionalContent'
+TextType_SubNumber = 'SubNumber'
+TextType_Date = 'Date'
 
 class TextParser:
 
+    def __init__(self, splitChar_str, rawContent_str):
+        self.splitChar = splitChar_str
+        self.rawContent = rawContent_str
+        self.splitContents = []
+        self.splitContentsParseTypeData = []
+        self.parsedIndex = 0
+    
+    def ParseTextBySpecificStructure(self, textStructureType_arr):
+        if textStructureType_arr.count(TextStructureType_Content) > 3:
+            print(f"[SNTest] TextStructureType = {TextStructureType_Content} is Over Limit(3) !")
+        elif textStructureType_arr.count(TextStructureType_Number) > 2:
+            print(f"[SNTest] TextStructureType = {TextStructureType_Number} is Over Limit(2) !")
+        elif textStructureType_arr.count(TextStructureType_Date) > 2:
+            print(f"[SNTest] TextStructureType = {TextStructureType_Date} is Over Limit(1) !")
+
+        self.splitContentsParseTypeData = []
+        self.splitContents = self.rawContent.split(self.splitChar)
+        self.parsedIndex = 0
+
+        while self.splitContents.count(''):
+            self.splitContents.remove('')
+
+        print(f"[SNTest] splitContents = {self.splitContents}")
+
+        splitContentsCount = len(self.splitContents)
+        self.splitContentsParseTypeData = ['']*splitContentsCount
+        specificStructureCount = len(textStructureType_arr)
+
+        if specificStructureCount <= 0 or splitContentsCount < specificStructureCount:
+            return None
+        
+        for textStructureType in textStructureType_arr:
+            isParseSuccess = self.ParseContentToStructureType(textStructureType)
+            if isParseSuccess == False:
+                return None
+        
+        print(f"[SNTest] origin splitContentsParseTypeData = {self.splitContentsParseTypeData}")
+        return self.RemoveRecordDataAndGetParseResult()
+
+    def RemoveRecordDataAndGetParseResult(self):
+        if  len(self.splitContents) <= 0 or \
+            len(self.splitContentsParseTypeData) <= 0 or\
+            len(self.splitContents) != len(self.splitContentsParseTypeData):
+            return None
+        
+        tempIndex = -1
+        tempContentArr = []
+        tempTextStructureTypeArr = []
+        canCombineText = False
+
+        while len(self.splitContentsParseTypeData) > 0:
+            parseTypeElement = self.splitContentsParseTypeData.pop(0)
+            splitContent = self.splitContents.pop(0)
+
+            if  parseTypeElement == TextStructureType_Number or \
+                parseTypeElement == TextStructureType_Date or \
+                parseTypeElement == TextStructureType_Content:
+                tempTextStructureTypeArr.append(parseTypeElement)
+                tempContentArr.append(splitContent)
+                tempIndex += 1
+                canCombineText = parseTypeElement == TextStructureType_Content
+            else:
+                if tempIndex < 0:
+                    return None
+                else:
+                    if canCombineText:
+                        tempContentArr[tempIndex] += f" {splitContent}"
+                    else:
+                        return None
+                    
+        
+        textTypeArr = []
+        numberTypeNum = 0
+        contentTypeNum = 0
+        dateTypeNum = 0
+        for index in range(len(tempTextStructureTypeArr)):
+            tempTextStructureType = tempTextStructureTypeArr[index]
+            if tempTextStructureType == TextStructureType_Number:
+                if numberTypeNum == 0:
+                    textTypeArr.append(TextType_Number)
+                elif numberTypeNum == 1:
+                    textTypeArr.append(TextType_SubNumber)
+                numberTypeNum += 1
+
+            elif tempTextStructureType == TextStructureType_Content:
+                if contentTypeNum == 0:
+                    textTypeArr.append(TextType_KeyWord)
+                elif contentTypeNum == 1:
+                    textTypeArr.append(TextType_SubContent)
+                elif contentTypeNum == 2:
+                    textTypeArr.append(TextType_AdditionalContent)
+                contentTypeNum += 1
+
+            elif tempTextStructureType == TextStructureType_Date:
+                if dateTypeNum == 0:
+                    textTypeArr.append(TextType_Date)
+                dateTypeNum += 1
+        
+        print(f'[SNTest] Parse Result combineArr = {tempContentArr}, textTypeArr = {textTypeArr}')
+        result = TextParseResult(tempContentArr, textTypeArr)
+        return result
+    
+    def ParseContentToStructureType(self, targetStructureType):
+        isResultSuccess = False
+        if len(self.splitContentsParseTypeData) <= 0 or len(self.splitContentsParseTypeData) != len(self.splitContents):
+            return isResultSuccess
+
+        if self.parsedIndex >= len(self.splitContents):
+            return isResultSuccess
+
+        for index in range(self.parsedIndex, len(self.splitContents)):
+            contentElement = self.splitContents[index]
+            isParseSuccess = \
+                targetStructureType == TextStructureType_Content or\
+                (targetStructureType == TextStructureType_Number and TextParser.CanValueConvertNumber(contentElement)) or\
+                (targetStructureType == TextStructureType_Date and TextParser.CanValueConvertDate(contentElement))
+
+            if isParseSuccess:
+                isResultSuccess = True
+                self.splitContentsParseTypeData[index] = targetStructureType
+                self.parsedIndex = index
+                break
+        
+        self.parsedIndex += 1
+        return isResultSuccess
+    
     def GetParseTextResult(self, splitChar_str, rawContent_str):
         self.splitChar = splitChar_str
         self.rawContent = rawContent_str
 
         splitContents = self.rawContent.split(self.splitChar)
-
         while splitContents.count(''):
             splitContents.remove('')
 
@@ -69,24 +200,28 @@ class TextParser:
             result = TextParseResult(newContents, textTypes)
             return result
     
-    def CanValueConvertNumberOrDate(value_var):
+    def CanValueConvertNumber(value_var):
         if isinstance(value_var, int):
             return True
-        elif isinstance(value_var, str):
-            if value_var.isdigit():
-                return True
-            elif dateManager.dateManager.CheckTextIsDateFormat(value_var):
-                return True
-        return False
+        elif isinstance(value_var, str) and value_var.isdigit():
+            return True
+        else:
+            return False
+    
+    def CanValueConvertDate(value_var):
+        if isinstance(value_var, str) and dateManager.dateManager.CheckTextIsDateFormat(value_var):
+            return True
+        else:
+            return False
 
 class TextParseResult:
 
-    def __init__(self, elements_arr, CombineType_arr):
+    def __init__(self, elements_arr, combineTypes_arr):
         self.elements = elements_arr
-        self.combineTypes = CombineType_arr
+        self.combineTypes = combineTypes_arr
     
     def PrintLog(self):
-        print(f'Elements = {self.elements}, CombineTypes = {self.combineTypes}')
+        print(f'[SNTest] TextParserResult Elements = {self.elements}, CombineTypes = {self.combineTypes}')
 
     def GetElementLength(self):
         return len(self.elements)
@@ -160,40 +295,3 @@ class TextParseResult:
             return True
         else:
             return False
-
-# rawContentArr = [
-#     ' ',
-#     '指令',
-#     '指令 ',
-#     '指令 1',
-#     '指令 A',
-#     '指令 A ',
-#     ' 指令 A ',
-#     '1 abc',
-#     '1 abc aa',
-#     '1 abc aa 5 aaw',
-#     ' 1 abc',
-#     ' 1 abasd 1',
-#     '1 atttt 5',
-#     '指令 1 A',
-#     ' 指令 1 A',
-#     '指令 A 1',
-#     '指令 A 1 B',
-#     '指令  A 1 B',
-#     '指令 Aqweq 15 Baa',
-#     '指令 Aqweq 15 Baa yyy',
-#     '指令 88 15 Baa yyy',
-#     '指令 A 1 ',
-#     '指令 AA BB CC 2',
-#     '5 AA BB CC 2']
-
-# findArr = [TextType_KeyWord, TextType_Number]
-
-# for c in rawContentArr:
-#     print(f'input : "{c}"')
-
-#     textParser = TextParser()
-#     result = textParser.GetParseTextResult(DEFAULT_SPLIT_CHAR, c)
-#     result.PrintLog()
-#     print(f'IsStructureMatch({findArr}) = {result.IsStructureMatch(findArr)}, IsStructureElementAllValid = {result.IsStructureElementAllValid(findArr)}')
-#     print(' ')

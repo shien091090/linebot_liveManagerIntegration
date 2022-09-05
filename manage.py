7a4a1a3f344ed665fedf125d3cf547ef9a878380
NKShien_Home <shien091091@gmail.com>
2022-09-05 21:45:38 +0800

    1.統一指令列表與指令說明  2.微重構

--- manage.py
+++ manage.py
@@ -62,161 +62,148 @@ def receiveMessage(event):
     print(f'[SNTest] Command Key = {commandKey}, Command Key type is {commandKeyTypeLog}')
     print('---------------------------------')
 
+    tempCommandKey = ""
+
     #指令列表
-    if commandKey == KeyWordSetting.keyWordEnum['KEY_COMMAND_LIST']:
+    tempCommandKey = 'KEY_COMMAND_LIST'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         reqInfo = RequestInfo(
-            KeyWordSetting.TITLE_COMMAND_LIST,
+            KeyWordSetting.GetCommandTitle(tempCommandKey),
             REQUEST_TYPE_BYPASS,
             None)
         reqInfo.statusMsg = '可使用的指令如下:'
-        reqInfo.resposeMsg = KeyWordSetting.getCommandKeyList()
+        reqInfo.resposeMsg = KeyWordSetting.GetCommandExplantion()
 
     #新增待辦事項
-    if commandKey == KeyWordSetting.keyWordEnum['KEY_MEMO_ADD']:
-        print('[SNTest] Check Command Format')
+    tempCommandKey = 'KEY_MEMO_ADD'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         commandTextStructure = [TextStructureType_Content, TextStructureType_Content]
         textParseResult = textParser.ParseTextBySpecificStructure(commandTextStructure)
 
         if textParseResult is None:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_ADD, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_MEMO_ADD']} <內容文字>'"
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint[tempCommandKey]}'"
             reqInfo.resposeMsg = ' '
         else:
             textParseResult.PrintLog()
             sendParam["action"] = lineActionInfo.API_ACTION_MEMO_ADD
             sendParam["subContent"] = textParseResult.GetSpecificTextTypeValue(TextType_SubContent)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_ADD, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
     
     #刪除待辦事項
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_MEMO_REMOVE']:
+    tempCommandKey = 'KEY_MEMO_REMOVE'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord, TextType_Number]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_REMOVE, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_MEMO_REMOVE']} <編號數字>'"
-            reqInfo.resposeMsg = ' '
-        elif textParseResult.IsStructureElementAllValid(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_REMOVE, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = '[輸入內容錯誤] 輸入內容不可為空或是無效文字'
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_MEMO_REMOVE
             sendParam["number"] = textParseResult.GetSpecificTextTypeValue(TextType_Number)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_REMOVE, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #修改待辦事項
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_MEMO_MODIFY']:
+    tempCommandKey = 'KEY_MEMO_MODIFY'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord, TextType_Number, TextType_SubContent]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_MODIFY, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_MEMO_MODIFY']} <編號數字> <新內容文字>'"
-            reqInfo.resposeMsg = ' '
-        elif textParseResult.IsStructureElementAllValid(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_MODIFY, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = '[輸入內容錯誤] 輸入內容不可為空或是無效文字'
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_MEMO_MODIFY
             sendParam["number"] = textParseResult.GetSpecificTextTypeValue(TextType_Number)
             sendParam["subContent"] = textParseResult.GetSpecificTextTypeValue(TextType_SubContent)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_MODIFY, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #確認待辦事項
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_MEMO_GET']:
+    tempCommandKey = 'KEY_MEMO_GET'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_GET, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_MEMO_GET']}'"
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_MEMO_GET
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_MEMO_GET, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #新增週期行程
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_SCHEDULE_ADD']:
+    tempCommandKey = 'KEY_SCHEDULE_ADD'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord, TextType_SubContent, TextType_Number, TextType_AdditionalContent]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_ADD, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_SCHEDULE_ADD']} <每月/每週/每天> <幾號/星期幾> <內容文字>'"
-            reqInfo.resposeMsg = ' '
-        elif textParseResult.IsStructureElementAllValid(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_ADD, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = '[輸入內容錯誤] 輸入內容不可為空或是無效文字'
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_SCHEDULE_ADD
             sendParam["subContent"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_SubContent)
             sendParam["number"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_Number)
             sendParam["additionalContent"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_AdditionalContent)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_ADD, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #刪除週期行程
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_SCHEDULE_REMOVE']:
+    tempCommandKey = 'KEY_SCHEDULE_REMOVE'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord, TextType_Number]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_REMOVE, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_SCHEDULE_REMOVE']} <編號數字>'"
-            reqInfo.resposeMsg = ' '
-        elif textParseResult.IsStructureElementAllValid(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_REMOVE, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = '[輸入內容錯誤] 輸入內容不可為空或是無效文字'
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_SCHEDULE_REMOVE
             sendParam["number"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_Number)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_REMOVE, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #修改週期行程
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_SCHEDULE_MODIFY']:
+    tempCommandKey = 'KEY_SCHEDULE_MODIFY'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord, TextType_Number, TextType_SubContent]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_MODIFY, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_SCHEDULE_MODIFY']} <編號數字> <新內容文字>'"
-            reqInfo.resposeMsg = ' '
-        elif textParseResult.IsStructureElementAllValid(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_MODIFY, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = '[輸入內容錯誤] 輸入內容不可為空或是無效文字'
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_SCHEDULE_MODIFY
             sendParam["number"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_Number)
             sendParam["subContent"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_SubContent)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_MODIFY, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #確認週期行程
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_SCHEDULE_GET']:
+    tempCommandKey = 'KEY_SCHEDULE_GET'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_GET, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_SCHEDULE_GET']}'"
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_SCHEDULE_GET
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_SCHEDULE_GET, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     #記帳
-    elif commandKey == KeyWordSetting.keyWordEnum['KEY_BUY']:
+    tempCommandKey = 'KEY_BUY'
+    if commandKey == KeyWordSetting.GetCommandKey(tempCommandKey):
         checkTextTypeStructure = [TextType_KeyWord, TextType_SubContent, TextType_Number]
 
         if textParseResult.IsStructureMatch(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_BUY, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.keyWordEnum['KEY_BUY']} <記帳項目> <價格>'"
-            reqInfo.resposeMsg = ' '
-        elif textParseResult.IsStructureElementAllValid(checkTextTypeStructure) == False:
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_BUY, REQUEST_TYPE_BYPASS, None)
-            reqInfo.statusMsg = '[輸入內容錯誤] 輸入內容不可為空或是無效文字'
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_BYPASS, None)
+            reqInfo.statusMsg = f"[格式錯誤] 正確格式為 '{KeyWordSetting.GetCommandFormatHint(tempCommandKey)}'"
             reqInfo.resposeMsg = ' '
         else:
             sendParam["action"] = lineActionInfo.API_ACTION_BUY
             sendParam["subContent"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_SubContent)
             sendParam["number"] = textParseResult.GetSpecificTextTypeValue(TextParserManager.TextType_Number)
-            reqInfo = RequestInfo(KeyWordSetting.TITLE_BUY, REQUEST_TYPE_GAS, sendParam)
+            reqInfo = RequestInfo(KeyWordSetting.GetCommandTitle(tempCommandKey), REQUEST_TYPE_GAS, sendParam)
 
     if reqInfo is None:
         print('[SNTest] Invalid Command')

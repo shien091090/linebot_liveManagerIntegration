cfcdc2c90021c7d94fb22090ae358911740c267c
NKShien_Home <shien091091@gmail.com>
2022-10-17 21:27:29 +0800

    修正Line指令跳錯

--- schedulerBroadcast.py
+++ schedulerBroadcast.py
@@ -8,15 +8,17 @@ import settings
 
 LINE_MAIN_GROUP_ID = 'Cd6af810de75bfc7bc6817373a1fd0562'
 TITLE_DAILY_REMIND = '每日提醒'
-line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
-sendParam = {}
-sendParam["action"] = API_ACTION_DAILY_SCHEDULER
-reqInfo = RequestInfo(TITLE_DAILY_REMIND, REQUEST_TYPE_GAS, sendParam)
-reqInfo.sendRequest()
+def DailyBroadCast():
+    line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
-replyFlexMessage = flexMessageManager.getFlexMessage(reqInfo.title, reqInfo.statusMsg, reqInfo.resposeMsg)
-flexMessageJsonDict = json.loads(replyFlexMessage)
+    sendParam = {}
+    sendParam["action"] = API_ACTION_DAILY_SCHEDULER
+    reqInfo = RequestInfo(TITLE_DAILY_REMIND, REQUEST_TYPE_GAS, sendParam)
+    reqInfo.sendRequest()
 
-pushText = FlexSendMessage(alt_text=reqInfo.title, contents=flexMessageJsonDict)
-line_bot_api.push_message(LINE_MAIN_GROUP_ID, messages = pushText)
+    replyFlexMessage = flexMessageManager.getFlexMessage(reqInfo.title, reqInfo.statusMsg, reqInfo.resposeMsg)
+    flexMessageJsonDict = json.loads(replyFlexMessage)
+
+    pushText = FlexSendMessage(alt_text=reqInfo.title, contents=flexMessageJsonDict)
+    line_bot_api.push_message(LINE_MAIN_GROUP_ID, messages = pushText)

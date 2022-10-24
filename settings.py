aef95ce14cd9ecbd5d567bea965d1f2081988036
NKShien_Home <shien091091@gmail.com>
2022-10-24 21:33:45 +0800

    更新API URL

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 LINE_CHANNEL_ACCESS_TOKEN = 'sPl0gKPsWSuq0Av7Us2F4Km8dLwDSuiYW75NmR9mpCRdug5Z9+a5psZ5iK/c3rgjdQfXp2L9iKi5vMG3QtRoAcT/G32RMhbHVUdMJZy1zeQ2D+7B7VHvMKnFLM/dZMe5zc/ukUCZ4YlA9j3lLA16/gdB04t89/1O/w1cDnyilFU='
 LINE_CHANNEL_SECRET = 'ab8d822858f40fa8a92abf575061211a'
-URL_GAS_API = 'https://script.google.com/macros/s/AKfycbxtg7U4p0si8_erffdxGzkvjv3_QyuAcLN4crpea4kO6vrRmZAEG6XO87EjCj8eTwtjAg/exec'
+URL_GAS_API = 'https://script.google.com/macros/s/AKfycbyqBZfiqvCPmVMMs3LtWLBZ88LJzDb3w9No8BKHgdsKpETG_vubWgzLl_7DaQMkvtZW/exec'

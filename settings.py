253490e224a1555309851eabe4adf0622ae5df7a
NK.Shien <lithoshu@yile.com.tw>
2023-03-25 12:45:50 +0800

    更新API URL

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 LINE_CHANNEL_ACCESS_TOKEN = 'sPl0gKPsWSuq0Av7Us2F4Km8dLwDSuiYW75NmR9mpCRdug5Z9+a5psZ5iK/c3rgjdQfXp2L9iKi5vMG3QtRoAcT/G32RMhbHVUdMJZy1zeQ2D+7B7VHvMKnFLM/dZMe5zc/ukUCZ4YlA9j3lLA16/gdB04t89/1O/w1cDnyilFU='
 LINE_CHANNEL_SECRET = 'ab8d822858f40fa8a92abf575061211a'
-URL_GAS_API = 'https://script.google.com/macros/s/AKfycby_xlaWDhT7VD-dkvgHmjbDqsQyjJQ1E-KVfkW3XjhOCrjqMXIHoevIY0oedje9lei_SQ/exec'
+URL_GAS_API = 'https://script.google.com/macros/s/AKfycbysxyFiCmIlDJoi_Qnhp99_72rinHlt7uAGqJ-EIrwiRYO7zlsSUH5EL9H3q88nsiDVMA/exec'

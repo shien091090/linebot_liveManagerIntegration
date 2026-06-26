0c15c01411d98611950082eb43e309f9b1030b9f
NK.Shien <lithoshu@yile.com.tw>
2026-06-26 09:11:36 +0800

    [feat] [每日推播] 新增一條訊息顯示家庭總覽網頁連結按鈕

--- schedulerBroadcast.py
+++ schedulerBroadcast.py
@@ -1,6 +1,6 @@
 from linebot import LineBotApi
 from linebot.models import FlexSendMessage
-from flexMessageManager import getMemoFlexMessage
+from flexMessageManager import getMemoFlexMessage, getUrlButtonFlexMessage
 from dateColorHelper import build_colored_memo_items, taiwan_today
 from lineActionInfo import RequestInfo, API_ACTION_DAILY_SCHEDULER
 from manage import REQUEST_TYPE_GAS
@@ -9,6 +9,7 @@ import settings
 
 LINE_MAIN_GROUP_ID = 'Cd6af810de75bfc7bc6817373a1fd0562'
 TITLE_DAILY_REMIND = '每日提醒'
+DASHBOARD_URL = 'https://linebot-livemanagerintegration.herokuapp.com/dashboard'
 
 
 def DailyBroadCast():
@@ -20,10 +21,12 @@ def DailyBroadCast():
 
     colored_items = build_colored_memo_items(req_info.responseMsg, taiwan_today())
     reply_flex_message = getMemoFlexMessage(req_info.title, req_info.statusMsg, colored_items)
-    flex_message_json_dict = json.loads(reply_flex_message)
+    push_daily = FlexSendMessage(alt_text=req_info.title, contents=json.loads(reply_flex_message))
 
-    push_text = FlexSendMessage(alt_text=req_info.title, contents=flex_message_json_dict)
-    line_bot_api.push_message(LINE_MAIN_GROUP_ID, messages=push_text)
+    dashboard_flex = getUrlButtonFlexMessage('家庭總覽', '點擊查看家庭狀況', DASHBOARD_URL)
+    push_dashboard = FlexSendMessage(alt_text='家庭總覽', contents=json.loads(dashboard_flex))
+
+    line_bot_api.push_message(LINE_MAIN_GROUP_ID, messages=[push_daily, push_dashboard])
 
 
 DailyBroadCast()
